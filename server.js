49e575674b0e2a10b4ff8d21157454a41384a8ed
Min-Huang <minh.huang96@gmail.com>
2019-10-09 08:41:01 -0700

    added put and delete request

--- server.js
+++ server.js
@@ -10,32 +10,96 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    db('cars').then(cars => {
-        res.status(200).json(cars)
-    }).catch(error => {
-        res.status(500).json(error)
-    })
+    db("cars")
+      .then(cars => {
+        res.status(200).json(cars);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: error,
+          message: "These cars information could not be retrieved."
+        });
+      });
 })
 
 server.get("/:id", (req, res) => {
     const { id } = req.params;
 
-  db("cars").where({id}).first()
+  db("cars")
+    .where({ id })
+    .first()
     .then(car => {
       res.status(200).json(car);
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({
+        error: error,
+        message: "This car information could not be retrieved."
+      });
     });
 });
 
 server.post('/', (req, res) => {
-    db('cars').insert(req.body).then(ids => {
-        db('cars').where({ id: ids[0] }).then(newCar => {
-            res.status(201).json(req.body)
-        })
-    }).catch(error => {
-        res.status(500).json(error)
-    })
+    db("cars")
+      .insert(req.body)
+      .then(ids => {
+        db("cars")
+          .where({ id: ids[0] })
+          .then(newCar => {
+            res.status(201).json({ id: ids, newData: req.body });
+          });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: error,
+          message: "The car information could not be saved."
+        });
+      });
 })
+
+server.put("/:id", (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      res
+        .status(200)
+        .json({ count: count, id: req.params.id, updatedData: req.body });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "This car information could not be modified."
+      });
+    });
+});
+
+server.delete("/:id",  (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json({
+        count: count,
+        message: `The data with ID: ${req.params.id} was removed`
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "This car information could not be removed."
+      });
+    });
+});
+
+function validateCarInfo(req, res, next) {
+  if (!req.body) {
+    res.status(400).json({ message: "please provide the car info" });
+  } else if (!req.body.VIN || !req.body.Make ||!req.body.Model || !req.body.Mileage) {
+    res.status(400).json({ message: "missing required VIN, Make, Model, or/and Mileage" });
+  } else {
+    next();
+  }
+}
+
 module.exports = server;
